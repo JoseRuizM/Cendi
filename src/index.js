2963f94fa51744829e1c8ff4e238cbd82c38e628
JoseRuiz <ruizmoranantonio@gmail.com>
2020-10-29 11:49:39 -0600

    Pull Test Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require('./config/server');
 
 //En el folder routes se encuentra userRoutes.js el cual se requiere aqui y recibe el object App
+//Modified
 require('./app/routes/index')(app); //Verificar si lo puedo mandar a server.js
 
 //starting server
